f76fa05eca7eccdb6508b04d15f7f696a90c444a
Barış SAVAŞ <barissavas17@gmail.com>
2021-10-30 00:00:50 +0300

    challenge solved

---
+++ structy_info1.js
@@ -0,0 +1,44 @@
+/* const depthFirstPrint = (arr, source) => {
+console.log(source )
+  for (const iterator of array[source]) {
+    depthFirstPrint(array, iterator)
+  }
+}
+const array = {
+  a:['b','c'],
+  b:['d'],
+  c:['e'],
+  d:['f'],
+  e:[],
+  f:[]
+}
+depthFirstPrint(array, 'a') */
+
+                                            // ----------------------------------- //
+                                            
+/* const breadthFirstPrint = (graph, source) => {
+  const queue = [source];
+  while (queue.length > 0) {
+    const current = queue.shift();
+    current
+    for ( let iterator of graph[current] ) {
+      queue.push(iterator)
+    }
+  }
+}
+
+const graph = {
+  a:['b','c'],
+  b:['d'],
+  c:['e'],
+  d:['f'],
+  e:[],
+  f:[]
+}
+
+breadthFirstPrint(graph, 'a') */
+
+
+
+
+
---
+++ undirectedpath_problem39.js
@@ -0,0 +1,13 @@
+const undirectedPath = (edges, nodeA, nodeB) => {
+
+}
+
+const edges = [
+    ['i', 'j'],
+    ['k', 'i'],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n']
+];
+
+console.log(undirectedPath(edges, 'j', 'm'));
