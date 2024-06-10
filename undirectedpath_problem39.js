c947eaa3951057f42418dade8bf857d5a8244918
Barış SAVAŞ <barissavas17@gmail.com>
2024-06-10 22:45:19 +0300

    solved

--- undirectedpath_problem39.js
+++ undirectedpath_problem39.js
@@ -1,40 +1,40 @@
 const undirectedPath = (edges, nodeA, nodeB) => {
-    const graph = buildGraph(edges);
-    return hasPath(graph, nodeA, nodeB, new Set());
-  };
-  
-  const buildGraph = (edges) => {
-    const graph = {};
-    for (let edge of edges) {
-      const [ a, b ] = edge;
-      if (!(a in graph)) graph[a] = [];
-      if (!(b in graph)) graph[b] = [];
-      graph[a].push(b);
-      graph[b].push(a);
+  const graph = buildGraph(edges);
+  return hasPath(graph, nodeA, nodeB, new Set());
+};
+
+const buildGraph = (edges) => {
+  const graph = {};
+  for (let edge of edges) {
+    const [a, b] = edge;
+    if (!(a in graph)) graph[a] = [];
+    if (!(b in graph)) graph[b] = [];
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  return graph;
+};
+
+const hasPath = (graph, src, dst, visited) => {
+  if (src === dst) return true;
+  if (visited.has(src)) return false;
+  visited.add(src);
+
+  for (let neighbor of graph[src]) {
+    if (hasPath(graph, neighbor, dst, visited) === true) {
+      return true;
     }
-    return graph;
-  };
-  
-  const hasPath = (graph, src, dst, visited) => {
-    if (src === dst) return true;
-    if (visited.has(src)) return false;
-    visited.add(src);
-    
-    for (let neighbor of graph[src]) {
-      if (hasPath(graph, neighbor, dst, visited) === true) {
-        return true;    
-      }
-    }
-    
-    return false;
-  };
+  }
+
+  return false;
+};
 
 const edges = [
-    ['i', 'j'],
-    ['k', 'i'],
-    ['m', 'k'],
-    ['k', 'l'],
-    ['o', 'n']
+  ["i", "j"],
+  ["k", "i"],
+  ["m", "k"],
+  ["k", "l"],
+  ["o", "n"],
 ];
 
-console.log(undirectedPath(edges, 'j', 'm'));
+console.log(undirectedPath(edges, "j", "m"));
